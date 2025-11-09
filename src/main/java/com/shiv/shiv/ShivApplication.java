6a39e56a7935659ac11c40219df25922012dc394
Sourabh1Dubey <155884072+Sourabh1Dubey@users.noreply.github.com>
2025-11-09 12:43:18 +0530

    live fronend

--- src/main/java/com/shiv/shiv/ShivApplication.java
+++ src/main/java/com/shiv/shiv/ShivApplication.java
@@ -18,7 +18,8 @@ public class ShivApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173") // React dev server
+                        .allowedOrigins("http://localhost:5173",
+                                "up-iapplication.vercel.app") // React dev server
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
             }
