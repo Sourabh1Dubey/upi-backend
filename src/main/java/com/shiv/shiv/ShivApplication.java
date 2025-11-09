1a431208a3bf2c38eaf5c025974d864c925416a4
Sourabh1Dubey <155884072+Sourabh1Dubey@users.noreply.github.com>
2025-11-09 20:07:20 +0530

    allUsers

--- src/main/java/com/shiv/shiv/ShivApplication.java
+++ src/main/java/com/shiv/shiv/ShivApplication.java
@@ -18,8 +18,8 @@ public class ShivApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173") // React dev server
-                       // .allowedOrigins("https://up-iapplication.vercel.app/")
+                        //.allowedOrigins("http://localhost:5173") // React dev server
+                        .allowedOrigins("https://up-iapplication.vercel.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
             }
